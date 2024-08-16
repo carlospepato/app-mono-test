4b5a2c9d8d4b60dca1d83f2a0cc465098b0d0ad2
carlospepato <71358695+carlospepato@users.noreply.github.com>
2024-08-16 16:58:29 -0300

    build adding jwtSecret

--- packages/api-node/config/config.ts
+++ packages/api-node/config/config.ts
@@ -5,9 +5,11 @@ dotenv.config();
 interface Config{
   port: number;
   dbUrl: string;
+  jwtSecret: string;
 }
 
 export const config : Config = {
   port: parseInt(process.env.PORT || '3000'),
-  dbUrl: process.env.DB_URL || ''
+  dbUrl: process.env.DB_URL || '',
+  jwtSecret: process.env.JWT_SECRET || 'DialogTimeline'
 }
