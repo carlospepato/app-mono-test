1c655ef231d18d21d5d89c55383bf8a54df5f38e
Carlos Pepato <kadubelber1@gmail.com>
2024-08-18 18:44:22 -0300

    feat cleaning files and making some settings such as background color in the body and font-family

--- packages/frontend-react/tailwind.config.js
+++ packages/frontend-react/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#0aea3e",
+        secondary: "#ffed4a",
+      }
+    },
   },
   plugins: [],
 }
