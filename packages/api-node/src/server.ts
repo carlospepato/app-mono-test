2d27c65134c5199d617e7e764835936fd0a31e11
carlospepato <71358695+carlospepato@users.noreply.github.com>
2024-08-19 17:40:02 -0300

    vercel config CORS

--- packages/api-node/src/server.ts
+++ packages/api-node/src/server.ts
@@ -12,11 +12,9 @@ import { authRoutes } from './routes/authRoutes';
 import { profileRoutes } from './routes/profileRoutes';
 import { timelineRoutes } from './routes/timelineRoutes';
 
-const allowedOrigins = process.env.FRONTEND_URL || 'http://localhost:5173';
-
 const server = Fastify({ logger: true });
 server.register(cors,{
-  origin: allowedOrigins,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 })
 
